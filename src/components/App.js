b8edb37c873027d3afab1f457d09fe13f94e05d9
sachin <12sachin95@gmail.com>
2021-12-18 23:13:26 +0530

    comited

--- src/components/App.js
+++ src/components/App.js
@@ -16,9 +16,6 @@ const App = ()=> {
 
   return (
     <>
-    <h1>hello from redux saga</h1>
-    <p>hello from redux</p>
-    <h3>hello from react</h3>
       {isloading && <h1>loading.....</h1>}
       {items && items.map((value,index)=>(
         <h1 key={index}>{value.name}</h1>
